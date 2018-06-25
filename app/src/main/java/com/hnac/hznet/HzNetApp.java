f5ccf0d0350c65e4fd26c6f596a70ef8866575ba
willow <yangwubo0800@gmail.com>
2018-06-25 15:36:28 +0800

    增加无网络和服务器不可用的情况下，加载页面失败的默认自定义页面显示； 返回键监听返回最初网页提示退出程序功能。

--- app/src/main/java/com/hnac/hznet/HzNetApp.java
+++ app/src/main/java/com/hnac/hznet/HzNetApp.java
@@ -1,12 +1,15 @@
 package com.hnac.hznet;
 
 import android.app.Application;
+import android.util.Log;
 
 public class HzNetApp extends Application {
-    public static boolean HzNet_DEBUG = false;
+    public static boolean HzNet_DEBUG = true;
+    private String TAG = "HzNetApp";
 
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG,"HzNetApp onCreate");
         HzNet_DEBUG = true;
     }
 }
--- app/src/main/java/com/hnac/hznet/MainActivity.java
+++ app/src/main/java/com/hnac/hznet/MainActivity.java
@@ -3,11 +3,12 @@ package com.hnac.hznet;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String  TAG = this.getClass().getName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             Intent it = new Intent();
+            Log.d(TAG,"=====myListener onclick");
             switch (view.getId()) {
                 case R.id.webview_page_load:
                     it.setClass(MainActivity.this, WebViewPageActivity.class);
